8c8d02d7b53e0ee4b557b02c5d6112b744265c56
Adarsh Kumar Rai <adarsh.rai@philips.com>
2018-02-21 09:11:38 +0530

    Added Framework Target

---
+++ ExceptionLogger/ExceptionLogger.h
@@ -0,0 +1,19 @@
+//
+//  ExceptionLogger.h
+//  ExceptionLogger
+//
+//  Created by Adarsh Kumar Rai on 21/02/18.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExceptionLogger.
+FOUNDATION_EXPORT double ExceptionLoggerVersionNumber;
+
+//! Project version string for ExceptionLogger.
+FOUNDATION_EXPORT const unsigned char ExceptionLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExceptionLogger/PublicHeader.h>
+
+
